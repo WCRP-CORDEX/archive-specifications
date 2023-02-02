dad6749b219de5a518aa8ea7255dc4dcc8956fcd
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-02-02 13:01:16 +0100

    moved to html format

--- scripts/create-domain-plots.py
+++ scripts/create-domain-plots.py
@@ -132,7 +132,7 @@ def create_domain_section(template):
 
 
 if __name__ == "__main__":
-    for domain_id in df.index[0:1]:
+    for domain_id in df.index:
         print(domain_id)
         plot_domain(domain_id)
     create_domain_section(template=op.join(bookpath, "domains.tpl"))
